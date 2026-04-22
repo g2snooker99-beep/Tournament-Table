98bfa13e012deae604d1b46cf2df2355b06a22e4
g2snooker99-beep <g2snooker99@gmail.com>
2026-04-22 09:22:17 +0700

    Create firebase-config.js file with Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm77BRlMPzQixiJ3HnX9ueFS2HvXnPVa4",
+  authDomain: "tournament-table-45dca.firebaseapp.com",
+  projectId: "tournament-table-45dca",
+  storageBucket: "tournament-table-45dca.firebasestorage.app",
+  messagingSenderId: "762901573808",
+  appId: "1:762901573808:web:48e52292299c0944894b75"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+console.log("Firebase initialized successfully");
+console.log("Firestore database reference:", db);
